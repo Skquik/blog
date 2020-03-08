6778ed0e9d9b41ba59a19ea199f27a9e91b3560d
Paul Nallet <pnallet@pageup.fr>
2020-03-08 14:36:08 +0100

    First navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class=" container mx-auto ">
-    {{posts}}
+  <div class="container sm:mx-auto flex sm:bg-red-200 md:bg-blue-500 sm:justify-between flex-col sm:flex-row">
+    <div class="flex mx-auto sm:mx-0 flex-col sm:flex-row ">
+      <img class="rounded-full h-20 w-20 self-center" src="@/assets/image/moi.png" alt="">
+      <div class="my-auto sm:ml-6 flex flex-col items-center sm:items-start">
+        <div class="font-bold text-blue-600">Paul Nallet</div>
+        <div>Développeur web</div>
+      </div>
+    </div>  
+    <div class="flex sm:my-auto justify-center order-first sm:order-none p-4 sm:p-0">
+        <nav class="flex px-6 text-blue-600 font-bold">A propos</nav>
+        <nav class="flex px-6 text-gray-400">Blog</nav>
+    </div>
   </div>
 </template>
 
@@ -12,21 +22,10 @@ export default {
       title: "Bonjour c'est Paul",
     }
   },
-  async asyncData({$prismic, error}){
+  async asyncData({ error}){
     try{
       // Query to get blog home content
-      const homepageContent = (await $prismic.api.getSingle('home')).data
-
-      // Query to get posts content to preview
-      const blogPosts = await $prismic.api.query(
-        $prismic.predicates.at("document.type", "blog-post")
-      )
-
-      // Returns data to be used in template
-      return {
-        homepageContent,
-        posts: blogPosts.results,
-      }
+      
     } catch (e) {
       console.log(e)
       // Returns error page
